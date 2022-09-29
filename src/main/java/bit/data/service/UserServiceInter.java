5ba70470326a44189e9b06a357ab454712524198
LAPTOP-FGR4PAPM\dmltk <dmltk1717@naver.com>
2022-09-29 14:57:01 +0900

    UserDaoInter, UserServiceInter 생성

--- src/main/java/bit/data/service/UserServiceInter.java
+++ src/main/java/bit/data/service/UserServiceInter.java
@@ -1,21 +1,30 @@
-package bit.data.dao;
-
+package bit.data.service;
 
 import bit.data.dto.UserDto;
 
 import java.util.List;
-import java.util.Map;
 
-public interface UserDaoInter {
+public interface UserServiceInter {
+
     public int getTotalCount();
+
     public int getLoginIdSearch(String loginid);
+
     public void insertUser(UserDto dto);
+
     public List<UserDto> getAllUsers();
+
     public UserDto getDataByUserNum(int user_num);
+
     public String getName(String loginid);
-    public int getLoginIdPasswordCheck(Map<String, String> map);
+
+    public int getLoginIdPasswordCheck(String loginid,String password);
+
     public UserDto getDataByLoginId(String loginid);
-    public void updateProfilePhoto(Map<String, Object> map);
+
+    public void updateProfilePhoto(int user_num,String profilephoto);
+
     public void updateUser(UserDto dto);
+
     public void deleteUser(int user_num);
 }
