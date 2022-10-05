933dfd4b6a75e70ef44608dfe62cf71e7dbbffd3
Joon <birth961016@gmail.com>
2022-10-05 10:19:05 +0900

    qna 주석~! 전에있던 프로젝트파일 삭제

--- src/main/java/bit/data/service/QnaAnswerService.java
+++ src/main/java/bit/data/service/QnaAnswerService.java
@@ -1,40 +1,40 @@
-package bit.data.service;
-
-import bit.data.dao.QnaAnswerDaoInter;
-import bit.data.dto.QnaAnswerDto;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-public class QnaAnswerService implements QnaAnswerServiceInter {
-
-    @Autowired
-    QnaAnswerDaoInter daoInter;
-
-    @Override
-    public List<QnaAnswerDto> getAllAnswerList(int answer_num) {
-        return daoInter.getAllAnswerList(answer_num);
-    }
-
-    @Override
-    public void inserAnswer(QnaAnswerDto dto) {
-        daoInter.inserAnswer(dto);
-    }
-
-    @Override
-    public QnaAnswerDto getAnswer(int qna_num) {
-        return daoInter.getAnswer(qna_num);
-    }
-
-    @Override
-    public QnaAnswerDto getUserAnswer(int user_num) {
-        return daoInter.getUserAnswer(user_num);
-    }
-
-    @Override
-    public void deleteAnswer(int qna_num) {
-        daoInter.deleteAnswer(qna_num);
-    }
-}
+//package bit.data.service;
+//
+//import bit.data.dao.QnaAnswerDaoInter;
+//import bit.data.dto.QnaAnswerDto;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public class QnaAnswerService implements QnaAnswerServiceInter {
+//
+//    @Autowired
+//    QnaAnswerDaoInter daoInter;
+//
+//    @Override
+//    public List<QnaAnswerDto> getAllAnswerList(int answer_num) {
+//        return daoInter.getAllAnswerList(answer_num);
+//    }
+//
+//    @Override
+//    public void inserAnswer(QnaAnswerDto dto) {
+//        daoInter.inserAnswer(dto);
+//    }
+//
+//    @Override
+//    public QnaAnswerDto getAnswer(int qna_num) {
+//        return daoInter.getAnswer(qna_num);
+//    }
+//
+//    @Override
+//    public QnaAnswerDto getUserAnswer(int user_num) {
+//        return daoInter.getUserAnswer(user_num);
+//    }
+//
+//    @Override
+//    public void deleteAnswer(int qna_num) {
+//        daoInter.deleteAnswer(qna_num);
+//    }
+//}
